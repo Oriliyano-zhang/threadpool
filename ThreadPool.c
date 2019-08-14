4186451bbca5b0c40ba291d1202d9c39fba14298
Oriliyano-zhang <45061909+Oriliyano-zhang@users.noreply.github.com>
2019-08-14 20:47:18 +0800

    Update ThreadPool.c

--- ThreadPool.c
+++ ThreadPool.c
@@ -14,45 +14,6 @@
 #define true 1
 #define false 0
 
-typedef struct {
-    void *(*function)(void *);          /* 函数指针，回调函数 */
-    void *arg;                          /* 上面函数的参数 */
-} threadpool_task_t;                    /* 各子线程任务结构体 */
-
-/* 线程池相关信息 */
-struct threadpool_t {
-    pthread_mutex_t lock;               /* 用于锁住本结构体 */    
-    pthread_mutex_t thread_counter;     /* 记录忙状态线程个数de琐 -- busy_thr_num */
-    pthread_cond_t queue_not_full;      /* 当任务队列满时，添加任务的线程阻塞，等待此条件变量 */
-    pthread_cond_t queue_not_empty;     /* 任务队列里不为空时，通知等待任务的线程 */
-
-    pthread_t *threads;                 /* 存放线程池中每个线程的tid。数组 */
-    pthread_t adjust_tid;               /* 存管理线程tid */
-    threadpool_task_t *task_queue;      /* 任务队列 */
-
-    int min_thr_num;                    /* 线程池最小线程数 */
-    int max_thr_num;                    /* 线程池最大线程数 */
-    int live_thr_num;                   /* 当前存活线程个数 */
-    int busy_thr_num;                   /* 忙状态线程个数 */
-    int wait_exit_thr_num;              /* 要销毁的线程个数 */
-
-    int queue_front;                    /* task_queue队头下标 */
-    int queue_rear;                     /* task_queue队尾下标 */
-    int queue_size;                     /* task_queue队中实际任务数 */
-    int queue_max_size;                 /* task_queue队列可容纳任务数上限 */
-
-    int shutdown;                       /* 标志位，线程池使用状态，true或false */
-};
-
-/*工作线程*/
-void *threadpool_thread(void *threadpool);
-
-/*管理线程*/
-void *adjust_thread(void *threadpool);
-
-int threadpool_free(threadpool_t *pool);
-int is_thread_alive(pthread_t tid);
-
 /*创建线程池*/
 threadpool_t *threadpool_create(int min_thr_num, int max_thr_num, int queue_max_size)
 {
@@ -253,6 +214,7 @@ void *adjust_thread(void *threadpool)
     return NULL;
 }
 
+/*销毁线程池*/
 int threadpool_destroy(threadpool_t *pool)
 {
     int i;
@@ -276,6 +238,7 @@ int threadpool_destroy(threadpool_t *pool)
     return 0;
 }
 
+/*释放资源*/
 int threadpool_free(threadpool_t *pool)
 {
     if (pool == NULL) {
@@ -299,7 +262,7 @@ int threadpool_free(threadpool_t *pool)
 
     return 0;
 }
-
+/*检查活着的工作线程*/
 int is_thread_alive(pthread_t tid)
 {
     int kill_rc = pthread_kill(tid, 0);     //发0号信号，测试线程是否存活
@@ -309,31 +272,4 @@ int is_thread_alive(pthread_t tid)
     return true;           
 }
 
-/*测试*/
-#if 1
-void *process(void *arg)
-{
-    printf("thread 0x%x working on task %d\n ",(unsigned int)pthread_self(),*(int *)arg);
-    sleep(1);
-    printf("task %d is end\n",*(int *)arg);
 
-    return NULL;
-}
-int main(void)
-{
-    threadpool_t *thp = threadpool_create(3,100,100);
-    printf("pool inited");
-
-    int num[20], i;
-    for (i = 0; i < 20; i++) {
-        num[i]=i;
-        printf("add task %d\n",i);
-        threadpool_add(thp, process, (void*)&num[i]);     /* 向线程池中添加任务 */
-    }
-    sleep(10);                                          /* 等子线程完成任务 */
-    threadpool_destroy(thp);
-
-    return 0;
-}
-
-#endif
