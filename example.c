a69469e6cbc72be8c8aad4b52b7eb85e374b3096
Oriliyano-zhang <1621409770@qq.com>
2019-08-14 04:54:56 -0700

    update file

---
+++ example.c
@@ -0,0 +1,33 @@
+#include "ThreadPool.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#if 1              
+
+void *process(void *arg)
+{
+    printf("thread 0x%x working on task %d\n ",(unsigned int)pthread_self(),*(int *)arg);
+    sleep(1);
+    printf("task %d is end\n",*(int *)arg);
+    return NULL; 
+}
+int main(void)
+{
+    threadpool_t *thp = threadpool_create(3,100,100);
+    printf("pool inited");
+
+    int num[20], i;
+    for (i = 0; i < 20; i++) {
+        num[i]=i;
+        printf("add task %d\n",i);
+        threadpool_add(thp, process, (void*)&num[i]);     /* 向线程池中添加任务 */
+
+    }
+    sleep(10);                                          /* 等子线程完成任务 */
+    threadpool_destroy(thp);
+
+    return 0;
+}
+#endif
